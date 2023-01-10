d172abbb87e6529ab71788a294b5dc99d71a0cf2
Syauqi <syauqisnh@gmail.com>
2023-01-10 20:18:26 +0700

    Penumpang Controller

---
+++ app/Http/Controllers/PenumpangController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Penumpang;
+use Illuminate\Http\Request;
+
+class PenumpangController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // $request all berfungsi untuk memasukkan semua request dari nama fiel
+        $penumpang = Penumpang::all();
+
+        return response()->json(
+            $data = [
+                'data' => $penumpang,
+
+            ]
+        );
+    }
+
+    public function store(Request $request)
+    {
+        // $request all berfungsi untuk memasukkan semua request dari nama field
+        $penumpang = new Penumpang;
+        $penumpang->nama = $request->nama;
+        $penumpang->alamat = $request->alamat;
+        $penumpang->umur = $request->umur;
+        $output = $penumpang->save();
+        return response()->json(
+            $data = [
+                'data' => $request->all(),
+            ]
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $penumpang = Penumpang::find($id);
+        return response()->json(
+            $data = [
+                'data' => $penumpang,
+            ]
+        );
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $penumpang = Penumpang::find($id);
+        $penumpang->nama = $request->nama;
+        $penumpang->alamat = $request->alamat;
+        $penumpang->umur = $request->umur;
+        $penumpang->save();
+        return response()->json(
+            $data = [
+                'data' => $request->all(),
+                'mahasiswa' => $penumpang
+            ]
+        );
+    }
+
+    public function destroy($id)
+    {
+        // $request all berfungsi untuk memasukkan semua request dari nama field
+        $penumpang = Penumpang::find($id);
+        $hapus = $penumpang->delete();
+
+        return response()->json(
+            $data = [
+                'data' => $hapus,
+
+            ]
+        );
+    }
+}
